67092a6d78acc1021835b52163e35a3177013ba4
shubham160305 <shubhambhagat16032005@gmail.com>
2025-12-29 16:42:02 +0530

    Delete src directory

--- src/components/icons/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './react-icon';
-export * from './nextjs-icon';
-export * from './nodejs-icon';
-export * from './tailwind-icon';
-export * from './typescript-icon';
-export * from './firebase-icon';
